dd401a600ed4ce8f2a336fd04f1950990fb82372
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-08-11 13:18:01 +0500

    Add files via upload

---
+++ словари и мн-ва.py
@@ -0,0 +1,18 @@
+my_disc = {'Екатерина': 1996, 'Иван': 1996, 'Алена': 1997}
+print(my_disc)
+print(my_disc['Алена'])
+print(my_disc.get('Диана'))
+my_disc.update({'Елена': 1986, 'Светлана': 2001})
+print(my_disc)
+a = my_disc.pop('Светлана')
+print(a)
+print(my_disc)
+my_set = {1, 5, 5, 1, 1, 2, 5, 2, 'Love', 1, 2, 3, 1.2556, 1.2556}
+print(my_set.add('Lion'))
+print(my_set.add((7,8,9)))
+print(my_set)
+print(my_set.remove(1))
+print(my_set)
+
+
+
